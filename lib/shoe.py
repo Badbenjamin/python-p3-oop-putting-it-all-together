a4f06c7625e7dca19d7f1815c43f2ec96800ba9d
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-27 22:01:58 -0400

    some tests passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size):
+        self.brand = brand
+        if type(size) == int:
+            self.size = size
+        else:
+            print("size must be an integer")
+
+    
+    def cobble(self, condition):
+        self.condition = new
+
+
+myshoe = Shoe("nike", 8)
+myshoe.cobble()
+print(myshoe.condition)
